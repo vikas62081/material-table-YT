da400f034fc4ada2d5b7fca8f4a37b3538e8e991
vikas62081 <vikas62081@gmail.com>
2021-02-06 11:36:48 +0530

    pushing populate data from external api

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
 
-const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', status: 0, role: 1 },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', status: 1, role: 0 },
-  { id: 3, name: "David", email: 'david342@gmail.com', status: 1, role: 3 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', status: 0, role: 2 },
-]
 
 function App() {
 
-  const [data, setData] = useState(empList)
+  const [data, setData] = useState([])
   const columns = [
     { title: "ID", field: "id" },
+    { title: "Username", field: "username" },
     { title: "Name", field: "name" },
-    { title: "Email", field: "email" }, 
-    { title: "Status", field: 'status', },
-    { title: "Role", field: "role", }
-  ] 
- 
+    { title: "Email", field: "email" },
+    { title: "Phone", field: "phone" },
+    { title: "Web Link", field: 'website' }
+  ]
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json())
+      .then(resp => {
+        setData(resp)
+      })
+  }, [])
 
   return (
     <div className="App">
