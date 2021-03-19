eacc2bf028eb2fdda1c834b58b0753847dd9a52d
vikas6208 <vikas62081@gmail.com>
2021-03-19 22:00:17 +0530

    row component overritten with overlay design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
+import CustomRow from './components/customRow';
 
 const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, city: "Bangalore" },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 9812345678, city: "Chennai" },
-  { id: 3, name: "David", email: 'david342@gmail.com', phone: 7896536289, city: "Jaipur" },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9087654321, city: "Hyderabad" },
+  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', gender: "Male", phone: 9876543210, city: "Bangalore" },
+  { id: 2, name: "Raj", email: 'raj@gmail.com', gender: "Male", phone: 9812345678, city: "Chennai" },
+  { id: 3, name: "David", email: 'david342@gmail.com', gender: "Male", phone: 7896536289, city: "Jaipur" },
+  { id: 4, name: "Sapna", email: 'sapna436@gmail.com', gender: "Female", phone: 9087654321, city: "Hyderabad" },
 ]
 
 function App() {
@@ -16,65 +17,25 @@ function App() {
     { title: "ID", field: "id", editable: false },
     { title: "Name", field: "name" },
     { title: "Email", field: "email" },
+    { title: "Gender", field: 'gender' },
     { title: "Phone Number", field: 'phone', },
     { title: "City", field: "city", }
   ]
 
-
+  const handleDelete=(index)=>{
+    alert(index)
+  }
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Material Table Bulk update</h4>
+      <h4 align='center'>Row overlay with supportive icons in Material table (Component Overriding)</h4>
       <MaterialTable
         title="Employee Data"
         data={data}
         columns={columns}
-        editable={{
-          onRowAdd: (newRow) => new Promise((resolve, reject) => {
-            const updatedRows = [...data, { id: Math.floor(Math.random() * 100), ...newRow }]
-            setTimeout(() => {
-              setData(updatedRows)
-              resolve()
-            }, 2000)
-          }),
-          onRowDelete: selectedRow => new Promise((resolve, reject) => {
-            const index = selectedRow.tableData.id;
-            const updatedRows = [...data]
-            updatedRows.splice(index, 1)
-            setTimeout(() => {
-              setData(updatedRows)
-              resolve()
-            }, 2000)
-          }),
-          onRowUpdate:(updatedRow,oldRow)=>new Promise((resolve,reject)=>{
-            const index=oldRow.tableData.id;
-            const updatedRows=[...data]
-            updatedRows[index]=updatedRow
-            setTimeout(() => {
-              setData(updatedRows)
-              resolve()
-            }, 2000)
-          }),
-          onBulkUpdate:selectedRows=>new Promise((resolve,reject)=>{
-            const rows=Object.values(selectedRows)
-            const updatedRows=[...data]
-            let index;
-            rows.map(emp=>{
-               index=emp.oldData.tableData.id
-               updatedRows[index]=emp.newData
-            })
-            setTimeout(()=>{
-              setData(updatedRows)
-              resolve()
-            },2000)
-          
-          })
-
-        }}
-        options={{
-          actionsColumnIndex: -1, addRowPosition: "first"
-        }}
-      />
+        components={{
+          Row: props => <CustomRow {...props} handleDelete={handleDelete}/>
+        }} />
     </div>
   );
 }
