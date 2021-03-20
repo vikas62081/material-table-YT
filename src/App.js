d23d45dda17229d53747c52223e7086f65254fdf
vikas6208 <vikas62081@gmail.com>
2021-03-20 16:23:15 +0530

    grouping in material table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
-import CustomRow from './components/customRow';
 
 const empList = [
   { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', gender: "Male", phone: 9876543210, city: "Bangalore" },
   { id: 2, name: "Raj", email: 'raj@gmail.com', gender: "Male", phone: 9812345678, city: "Chennai" },
-  { id: 3, name: "David", email: 'david342@gmail.com', gender: "Male", phone: 7896536289, city: "Jaipur" },
+  { id: 3, name: "David", email: 'david342@gmail.com', gender: "Male", phone: 7896536289, city: "Bangalore" },
   { id: 4, name: "Sapna", email: 'sapna436@gmail.com', gender: "Female", phone: 9087654321, city: "Hyderabad" },
 ]
 
@@ -14,28 +13,27 @@ function App() {
 
   const [data, setData] = useState(empList)
   const columns = [
-    { title: "ID", field: "id", editable: false },
+    { title: "ID", field: "id",grouping:false },
     { title: "Name", field: "name" },
     { title: "Email", field: "email" },
-    { title: "Gender", field: 'gender' },
-    { title: "Phone Number", field: 'phone', },
-    { title: "City", field: "city", }
+    { title: "Gender", field: 'gender',defaultGroupOrder:1},
+    {title:"Phone Number",field:"phone"},
+    {title:"City",field:'city',defaultGroupOrder:0,defaultGroupSort:"desc"}
   ]
 
-  const handleDelete=(index)=>{
-    alert(index)
-  }
+  
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Row overlay with supportive icons in Material table (Component Overriding)</h4>
+      <h4 align='center'>Grouping in Material table</h4>
       <MaterialTable
         title="Employee Data"
         data={data}
         columns={columns}
-        components={{
-          Row: props => <CustomRow {...props} handleDelete={handleDelete}/>
-        }} />
+        options={{
+          grouping:true
+        }}
+       />
     </div>
   );
 }
