edf0cd8f06ccefbe9646419e92721c4d346dedad
vikas6208 <vikas62081@gmail.com>
2021-03-25 20:22:02 +0530

    pushing import excel or csv data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,77 @@
 import React, { useState } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
-import CustomRow from './components/customRow';
+import XLSX from 'xlsx'
 
-const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', gender: "Male", phone: 9876543210, city: "Bangalore" },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', gender: "Male", phone: 9812345678, city: "Chennai" },
-  { id: 3, name: "David", email: 'david342@gmail.com', gender: "Male", phone: 7896536289, city: "Jaipur" },
-  { id: 4, name: "Sapna", email: 'sapna436@gmail.com', gender: "Female", phone: 9087654321, city: "Hyderabad" },
-]
-
+const EXTENSIONS=['xlsx','xls','csv']
 function App() {
+  const [colDefs, setColDefs] = useState()
+  const [data, setData] = useState()
 
-  const [data, setData] = useState(empList)
-  const columns = [
-    { title: "ID", field: "id", editable: false },
-    { title: "Name", field: "name" },
-    { title: "Email", field: "email" },
-    { title: "Gender", field: 'gender' },
-    { title: "Phone Number", field: 'phone', },
-    { title: "City", field: "city", }
-  ]
-
-  const handleDelete=(index)=>{
-    alert(index)
+  const getExention=(file)=>{
+const parts=file.name.split('.')
+const extension=parts[parts.length-1]
+return EXTENSIONS.includes(extension)
   }
+const convertToJson=(headers,data)=>{
+  const rows=[]
+  data.forEach(row => {
+let rowData={}
+    row.forEach((element,index)=>{
+rowData[headers[index]]=element
+    })
+   rows.push(rowData)
+    
+  });
+  return rows
+}
+
+  const importExcel=(e)=>{
+    const file=e.target.files[0]
+
+   const reader=new FileReader()
+   reader.onload=(event)=>{
+     //parse data
+
+     const bstr=event.target.result
+     const workBook=XLSX.read(bstr,{type:"binary"})
+     
+     //get first sheet
+     const workSheetName=workBook.SheetNames[0]
+     const workSheet=workBook.Sheets[workSheetName]
+    //convert to array
+     const fileData=XLSX.utils.sheet_to_json(workSheet,{header:1})
+// console.log(fileData)
+const headers=fileData[0]
+const heads=headers.map(head=>({title:head,field:head}))
+setColDefs(heads)
+
+//removing header
+fileData.splice(0,1)
+
+
+setData(convertToJson(headers,fileData))
+   }
+
+  if(file){
+    if(getExention(file)){
+      reader.readAsBinaryString(file)
+      }
+      else{
+        alert("Invalid file input, Select Excel, CSV file")
+      }
+  }else{
+    setData([])
+    setColDefs([])
+  }
+  }
+
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Row overlay with supportive icons in Material table (Component Overriding)</h4>
-      <MaterialTable
-        title="Employee Data"
-        data={data}
-        columns={columns}
-        components={{
-          Row: props => <CustomRow {...props} handleDelete={handleDelete}/>
-        }} />
+      <h4 align='center'>Import Data from Excel, CSV in Material Table</h4>
+      <input type="file" onChange={importExcel}/>
+      <MaterialTable title="Olympic Data" data={data} columns={colDefs} />
     </div>
   );
 }
