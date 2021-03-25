122d199b4eee6044da5c99a4caece6b5e40461d6
vikas6208 <vikas62081@gmail.com>
2021-03-25 20:23:20 +0530

    pushing import excel or csv data .

--- src/App.js
+++ src/App.js
@@ -3,74 +3,75 @@ import './App.css';
 import MaterialTable from 'material-table'
 import XLSX from 'xlsx'
 
-const EXTENSIONS=['xlsx','xls','csv']
+const EXTENSIONS = ['xlsx', 'xls', 'csv']
 function App() {
   const [colDefs, setColDefs] = useState()
   const [data, setData] = useState()
 
-  const getExention=(file)=>{
-const parts=file.name.split('.')
-const extension=parts[parts.length-1]
-return EXTENSIONS.includes(extension)
+  const getExention = (file) => {
+    const parts = file.name.split('.')
+    const extension = parts[parts.length - 1]
+    return EXTENSIONS.includes(extension) // return boolean
   }
-const convertToJson=(headers,data)=>{
-  const rows=[]
-  data.forEach(row => {
-let rowData={}
-    row.forEach((element,index)=>{
-rowData[headers[index]]=element
-    })
-   rows.push(rowData)
-    
-  });
-  return rows
-}
 
-  const importExcel=(e)=>{
-    const file=e.target.files[0]
+  const convertToJson = (headers, data) => {
+    const rows = []
+    data.forEach(row => {
+      let rowData = {}
+      row.forEach((element, index) => {
+        rowData[headers[index]] = element
+      })
+      rows.push(rowData)
 
-   const reader=new FileReader()
-   reader.onload=(event)=>{
-     //parse data
+    });
+    return rows
+  }
 
-     const bstr=event.target.result
-     const workBook=XLSX.read(bstr,{type:"binary"})
-     
-     //get first sheet
-     const workSheetName=workBook.SheetNames[0]
-     const workSheet=workBook.Sheets[workSheetName]
-    //convert to array
-     const fileData=XLSX.utils.sheet_to_json(workSheet,{header:1})
-// console.log(fileData)
-const headers=fileData[0]
-const heads=headers.map(head=>({title:head,field:head}))
-setColDefs(heads)
+  const importExcel = (e) => {
+    const file = e.target.files[0]
 
-//removing header
-fileData.splice(0,1)
+    const reader = new FileReader()
+    reader.onload = (event) => {
+      //parse data
+
+      const bstr = event.target.result
+      const workBook = XLSX.read(bstr, { type: "binary" })
+
+      //get first sheet
+      const workSheetName = workBook.SheetNames[0]
+      const workSheet = workBook.Sheets[workSheetName]
+      //convert to array
+      const fileData = XLSX.utils.sheet_to_json(workSheet, { header: 1 })
+      // console.log(fileData)
+      const headers = fileData[0]
+      const heads = headers.map(head => ({ title: head, field: head }))
+      setColDefs(heads)
+
+      //removing header
+      fileData.splice(0, 1)
 
 
-setData(convertToJson(headers,fileData))
-   }
+      setData(convertToJson(headers, fileData))
+    }
 
-  if(file){
-    if(getExention(file)){
-      reader.readAsBinaryString(file)
+    if (file) {
+      if (getExention(file)) {
+        reader.readAsBinaryString(file)
       }
-      else{
+      else {
         alert("Invalid file input, Select Excel, CSV file")
       }
-  }else{
-    setData([])
-    setColDefs([])
-  }
+    } else {
+      setData([])
+      setColDefs([])
+    }
   }
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
       <h4 align='center'>Import Data from Excel, CSV in Material Table</h4>
-      <input type="file" onChange={importExcel}/>
+      <input type="file" onChange={importExcel} />
       <MaterialTable title="Olympic Data" data={data} columns={colDefs} />
     </div>
   );
