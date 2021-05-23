f85fdbe40ef16bfea2ffe5f0170667061e9c7506
vikas6208 <vikas62081@gmail.com>
2021-05-23 16:24:10 +0530

    add summary row feature to table

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React from 'react';
 import './App.css';
-import MaterialTable from 'material-table'
-
+import MaterialTable, { MTablePagination } from 'material-table'
+import { TablePagination, Grid, Typography, Divider } from '@material-ui/core'
 const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210,age:23 },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234,age:17 },
-  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678,age:34 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321,age:20 },
+  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, age: 23 },
+  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17 },
+  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34 },
+  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20 }, { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17 },
+  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34 },
+  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20 },
 ]
 
 function App() {
   const columns = [
-  { title: "ID", field: "id" },
-    { title: "Name", field: "name"},
+    { title: "ID", field: "id" },
+    { title: "Name", field: "name" },
     { title: "Email", field: "email" },
     { title: "Phone Number", field: 'phone' },
-    {title:"Age",field:'age'}
-    ]   
+    { title: "Age", field: 'age' }
+  ]
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Hide or Disable action buttons conditionally in Material Table</h4>
+      <h4 align='center'>Add summary row feature to Material Table</h4>
       <MaterialTable
         title="Employee Data"
         data={empList}
-        columns={columns} 
-        editable={{
-          isDeleteHidden:(row)=>row.age<18,
-          isDeletable:(row)=>row.id%2===0,
-          isEditable:row=>row.id%2!==0,
-            onRowAdd:()=>(newdata)=>null,
-            onRowDelete:()=>(newdata)=>null,
-            onRowUpdate:()=>(newdata)=>null,
-  
-          }
-          
-            }/>
+        columns={columns}
+        components={{
+          Pagination: (props) => <>
+            <Grid container style={{ padding:15}}>
+              <Grid sm={6} item><Typography variant="subtitle2">Total</Typography></Grid>
+              <Grid sm={6} item align="center"><Typography variant="subtitle2" >Number of rows : {props.count}</Typography></Grid>
+            </Grid>
+            <Divider/>
+            <TablePagination {...props} />
+          </>
+        }}
+      />
     </div>
   );
 }
