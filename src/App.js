5c23f5ece46099093592fac5d40e255b9ff6d22b
vikas6208 <vikas62081@gmail.com>
2021-05-23 17:45:26 +0530

    adding dark mode to material table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import MaterialTable, { MTablePagination } from 'material-table'
-import { TablePagination, Grid, Typography, Divider } from '@material-ui/core'
+import MaterialTable from 'material-table'
+import { MuiThemeProvider, createMuiTheme, FormControlLabel, Switch } from '@material-ui/core'
 const empList = [
   { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, age: 23 },
   { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17 },
-  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20 }, { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17 },
   { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34 },
   { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20 },
 ]
 
 function App() {
+  const [preferDarkMode, setPreferDarkMode] = useState(() => {
+    const mode = localStorage.getItem('_tableDarkMode')
+    return mode === "true" || false
+  })
   const columns = [
     { title: "ID", field: "id" },
     { title: "Name", field: "name" },
@@ -19,26 +21,35 @@ function App() {
     { title: "Phone Number", field: 'phone' },
     { title: "Age", field: 'age' }
   ]
+  const theme = createMuiTheme({
+    palette: {
+      type: preferDarkMode ? 'dark' : 'light'
+    }
+  })
+
+  const handleDarkModeChange = () => {
+    setPreferDarkMode(!preferDarkMode)
+    localStorage.setItem('_tableDarkMode', !preferDarkMode)
+  }
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Add summary row feature to Material Table</h4>
-      <MaterialTable
-        title="Employee Data"
-        data={empList}
-        columns={columns}
-        components={{
-          Pagination: (props) => <>
-            <Grid container style={{ padding:15}}>
-              <Grid sm={6} item><Typography variant="subtitle2">Total</Typography></Grid>
-              <Grid sm={6} item align="center"><Typography variant="subtitle2" >Number of rows : {props.count}</Typography></Grid>
-            </Grid>
-            <Divider/>
-            <TablePagination {...props} />
-          </>
-        }}
+      <h4 align='center'>Dark theme in Material Table</h4>
+      <FormControlLabel
+        value="top"
+        control={<Switch color="primary" checked={preferDarkMode} />}
+        onChange={handleDarkModeChange}
+        label="Dark Mode"
+        labelPlacement="top"
       />
+      <MuiThemeProvider theme={theme}>
+        <MaterialTable
+          title="Employee Data"
+          data={empList}
+          columns={columns}
+        />
+      </MuiThemeProvider>
     </div>
   );
 }
