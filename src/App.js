65e3e529f4b0e6630a54747657a5d61baa057f3c
vikas6208 <vikas62081@gmail.com>
2021-05-23 15:49:14 +0530

    pushing conditional action button hide and disable

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
-import {Link} from '@material-ui/core'
+
 const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234 },
-  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321 },
+  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210,age:23 },
+  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234,age:17 },
+  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678,age:34 },
+  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321,age:20 },
 ]
 
 function App() {
   const columns = [
-  { title: "ID", field: "id",render:rowData=><Link href={`https://picsum.photos/1000?random=${rowData.id}`} target="_blank">{rowData.id}</Link> },
-    { title: "Name", field: "name",render:rowData=><Link href={`/user?id=${rowData.id}`} target="_blank">{rowData.name}</Link> },
+  { title: "ID", field: "id" },
+    { title: "Name", field: "name"},
     { title: "Email", field: "email" },
     { title: "Phone Number", field: 'phone' },
-    {title:"Profile",render:rowData=><Link href={`/user?id=${rowData.id}`} target="_blank">Profile</Link>}
+    {title:"Age",field:'age'}
     ]   
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Add hyperlink to column in Material Table</h4>
+      <h4 align='center'>Hide or Disable action buttons conditionally in Material Table</h4>
       <MaterialTable
         title="Employee Data"
         data={empList}
-        columns={columns} />
+        columns={columns} 
+        editable={{
+          isDeleteHidden:(row)=>row.age<18,
+          isDeletable:(row)=>row.id%2===0,
+          isEditable:row=>row.id%2!==0,
+            onRowAdd:()=>(newdata)=>null,
+            onRowDelete:()=>(newdata)=>null,
+            onRowUpdate:()=>(newdata)=>null,
+  
+          }
+          
+            }/>
     </div>
   );
 }
