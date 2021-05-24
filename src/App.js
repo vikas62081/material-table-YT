2c0d21a998557e70cb7ca27b054dfde3b409d693
vikas6208 <vikas62081@gmail.com>
2021-05-24 15:05:01 +0530

    adding filtering changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import MaterialTable, { MTableToolbar } from 'material-table'
-import { MuiThemeProvider, createMuiTheme, FormControlLabel, Switch, Grid, Typography, Divider } from '@material-ui/core'
+import MaterialTable from 'material-table'
+import {FormControlLabel,Checkbox,Select,MenuItem} from '@material-ui/core'
 const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, age: 23 },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17 },
-  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20 },
+  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', phone: 9876543210, age: 23, year: 2019 },
+  { id: 2, name: "Raj", email: 'raj@gmail.com', phone: 6678901234, age: 17, year: 2020 },
+  { id: 3, name: "David", email: 'david342@gmail.com', phone: 6312345678, age: 34, year: 2019 },
+  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', phone: 9787654321, age: 20, year: 2021 },
 ]
 
 function App() {
-  const [preferDarkMode, setPreferDarkMode] = useState(() => {
-    const mode = localStorage.getItem('_tableDarkMode')
-    return mode === "true" || false
-  })
+  const [filteredData,setFilteredData]=useState(empList)
+ const [filter,setFilter]=useState(false)
+ const [year,setYear]=useState('all')
+ useEffect(()=>{
+setFilteredData(year==="all"?empList:empList.filter(dt=>dt.year===year))
+ },[year])
   const columns = [
     { title: "ID", field: "id" },
     { title: "Name", field: "name" },
     { title: "Email", field: "email" },
     { title: "Phone Number", field: 'phone' },
-    { title: "Age", field: 'age' }
+    { title: "Age", field: 'age' },
+    { title: "Joining Year", field: 'year' }
   ]
-  const theme = createMuiTheme({
-    palette: {
-      type: preferDarkMode ? 'dark' : 'light'
-    }
-  })
-
-  const handleDarkModeChange = () => {
-    setPreferDarkMode(!preferDarkMode)
-    localStorage.setItem('_tableDarkMode', !preferDarkMode)
-  }
-
+const handleChange=()=>{
+  setFilter(!filter)
+}
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Override toolbar component in Material Table</h4>
-
-      <MuiThemeProvider theme={theme}>
-        <MaterialTable
-          title="Employee Data"
-          data={empList}
-          columns={columns}
-          components={{
-            Action: (props) => <FormControlLabel
-              value="top"
-              control={<Switch color="primary" checked={preferDarkMode} />}
-              onChange={handleDarkModeChange}
-              label="Dark Mode"
-              labelPlacement="top"
-            />,
-            Toolbar: (props) => <div>
-              <MTableToolbar {...props} />
-              <Grid style={{ padding: 15 }} align="right">
-                <Typography variant="subtitle2">Number of rows : {props.data.length}</Typography>
-              </Grid>
-              <Divider />
-            </div>
-          }}
-          actions={[
-            { isFreeAction: true }
-          ]}
-        />
-      </MuiThemeProvider>
+      <h4 align='center'>Filtering in Material Table</h4>
+      
+      <MaterialTable
+        title="Employee Data"
+        data={filteredData}
+        columns={columns}
+        options={{
+          filtering:filter
+        }}
+        components={{
+          Action:(props)=><>
+          <FormControlLabel
+        control={
+          <Checkbox
+            checked={filter}
+            onChange={handleChange}
+            name="checkedB"
+            color="secondary"
+          />
+        }
+        label="Filter"
+      />
+      <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          style={{width:100}}
+          value={year}
+          onChange={(e)=>setYear(e.target.value)}
+        >
+          <MenuItem value={'all'}><em>All</em></MenuItem>
+          <MenuItem value={2019}>2019</MenuItem>
+          <MenuItem value={2020}>2020</MenuItem>
+          <MenuItem value={2021}>2021</MenuItem>
+        </Select>
+        </>
+        }}
+        actions={[
+          {isFreeAction:true}
+        ]}
+      />
     </div>
   );
 }
