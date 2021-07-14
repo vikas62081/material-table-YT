056fd60220ffb3fa3f116e1eea0de369d2d698af
vikas6208 <vikas62081@gmail.com>
2021-07-14 13:04:40 +0530

    adding pdf export option

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
 import XLSX from 'xlsx'
+import PrintIcon from '@material-ui/icons/Print'
+import jsPDF from 'jspdf'
+import 'jspdf-autotable'
 const studentData = [
   {
     id: 1,
@@ -30,41 +33,60 @@ function App() {
   const columns = [
     { title: "Name", field: "name", },
     { title: "Email", field: "email", },
-    { title: "Year", field: "year",type:"numeric" },
-    { title: "Fee", field: 'fee',type:"currency" }]
+    { title: "Year", field: "year", type: "numeric" },
+    { title: "Fee", field: 'fee', type: "currency" }]
 
-    const downloadExcel=()=>{
-      const newData=studentData.map(row=>{
-        delete row.tableData
-        return row
-      })
-      const workSheet=XLSX.utils.json_to_sheet(newData)
-      const workBook=XLSX.utils.book_new()
-      XLSX.utils.book_append_sheet(workBook,workSheet,"students")
-      //Buffer
-      let buf=XLSX.write(workBook,{bookType:"xlsx",type:"buffer"})
-      //Binary string
-      XLSX.write(workBook,{bookType:"xlsx",type:"binary"})
-      //Download
-      XLSX.writeFile(workBook,"StudentsData.xlsx")
+  const downloadExcel = () => {
+    const newData = studentData.map(row => {
+      delete row.tableData
+      return row
+    })
+    const workSheet = XLSX.utils.json_to_sheet(newData)
+    const workBook = XLSX.utils.book_new()
+    XLSX.utils.book_append_sheet(workBook, workSheet, "students")
+    //Buffer
+    XLSX.write(workBook, { bookType: "xlsx", type: "buffer" })
+    //Binary string
+    XLSX.write(workBook, { bookType: "xlsx", type: "binary" })
+    //Download
+    XLSX.writeFile(workBook, "StudentsData.xlsx")
 
 
-    }
+  }
+  const downloadPdf = () => {
+    const doc = new jsPDF()
+    doc.text("Student Details", 20, 10)
+    doc.autoTable({
+      theme: "grid",
+      columns: columns.map(col => ({ ...col, dataKey: col.field })),
+      body: studentData
+    })
+    doc.save('table.pdf')
+  }
+
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Export Data to Excel in Material Table</h4>
+      <h4 align='center'>Export Data to Pdf in Material Table</h4>
       <MaterialTable
         title="Student Details"
         columns={columns}
         data={studentData}
         actions={[
-          {icon:()=><button>Export</button>,// you can pass icon too
-          tooltip:"Export to Excel",
-        onClick:()=>downloadExcel(),
-      isFreeAction:true}
+          {
+            icon: () => <button>Export</button>,// you can pass icon too
+            tooltip: "Export to Excel",
+            onClick: () => downloadExcel(),
+            isFreeAction: true
+          },
+          {
+            icon: () => <PrintIcon />,// you can pass icon too
+            tooltip: "Export to Pdf",
+            onClick: () => downloadPdf(),
+            isFreeAction: true
+          }
         ]}
-        />
+      />
     </div>
   );
 }
