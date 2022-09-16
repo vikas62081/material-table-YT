44619298f8e9b6bb8ccdc2b250ba40a3ad7b18a8
vikas62081 <vikas62081@gmail.com>
2022-09-16 20:37:08 +0530

    row selection

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import MaterialTable from 'material-table'
-
-const empList = [
-  { id: 1, name: "Neeraj", email: 'neeraj@gmail.com', status: 0, role: 1 },
-  { id: 2, name: "Raj", email: 'raj@gmail.com', status: 1, role: 0 },
-  { id: 3, name: "David", email: 'david342@gmail.com', status: 1, role: 3 },
-  { id: 4, name: "Vikas", email: 'vikas75@gmail.com', status: 0, role: 2 },
-]
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import MaterialTable from "material-table";
 
 function App() {
-
-  const [data, setData] = useState(empList)
+  const [data, setData] = useState([]);
+  const [selectedRow, setSelectedRow] = useState();
   const columns = [
-    { title: "ID", field: "id" },
+    { title: "ID", field: "id", hidden: true },
+    { title: "Username", field: "username", hiddenByColumnsButton: true },
     { title: "Name", field: "name" },
-    { title: "Email", field: "email" }, 
-    { title: "Status", field: 'status', },
-    { title: "Role", field: "role", }
-  ] 
- 
+    { title: "Email", field: "email" },
+    { title: "Phone", field: "phone" },
+    { title: "Web Link", field: "address.street" },
+  ];
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((resp) => resp.json())
+      .then((resp) => {
+        setData(resp);
+      });
+  }, []);
+
+  const onSelectionChange = (selectedRows) => {
+    console.log(selectedRows);
+  };
+  const onRowClick = (e, clickedRow) => {
+    setSelectedRow(clickedRow);
+  };
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      <h4 align='center'>Material Table</h4>
+      <h4 align="center">
+        Learn Row Selection (single/ mutiple) in Material Table
+      </h4>
       <MaterialTable
         title="Employee Data"
         data={data}
         columns={columns}
+        onRowClick={onRowClick}
+        options={{
+          //For multi select
+          // selection: true,
+          // showSelectAllCheckbox: false,
+          // showTextRowsSelected: false,
+          rowStyle: (row) =>
+            row?.id === selectedRow?.id ? { background: "#e7e7e7" } : {},
+        }}
+        onSelectionChange={onSelectionChange}
       />
+      {selectedRow && <h4>Selected Employee Name : {selectedRow?.name}</h4>}
     </div>
   );
 }
